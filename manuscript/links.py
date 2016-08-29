98b020870ff0dbf05d35da79eb374cc91a1d65a5
Suraj Patil <sapatil@live.com>
2016-08-29 23:18:04 +0530

    add the next section previous section for github browsing

---
+++ manuscript/links.py
@@ -0,0 +1,15 @@
+books = open("Book.txt","r")
+lines = books.readlines()
+lines = [line.strip("\n") for line in lines]
+
+for i in range(len(lines)):
+	i += 1
+	cur = i
+	curfile = open(lines[i], "a")
+	prev = i - 1
+	if i != len(lines) -1 :
+		nxt = i+1
+	curfile.write("\n#####Links\n")
+	curfile.write("-[Previous section](manuscript/"+lines[prev]+")\n")
+	if i != len(lines) - 1:
+		curfile.write("-[Next section](manuscript/"+lines[nxt]+")\n")
